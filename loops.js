62f75f459cc6e24c367e5af3c3d62b4e23e03b0b
Sasha Hart <sashanicolehart@gmail.com>
2018-08-26 01:42:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -42,10 +42,6 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  var i = array.lenth;
-  function incrementVariable() {
-    i--;
-  }
   do {
     array.pop();
   } while (array.length > 0 && incrementVariable());
